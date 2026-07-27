9fe94734c899c512274e7bbc2db79bf366efb560
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-07-27 13:08:50 +0530

    Time: 837 ms (13.2%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT(e1.employee_id), e1.name, COUNT(e2.reports_to) as reports_count, ROUND(AVG(e2.age)) as average_age 
+SELECT e1.employee_id, e1.name, COUNT(e2.reports_to) as reports_count, ROUND(AVG(e2.age)) as average_age 
 FROM Employees as e1
 Join Employees as e2
 ON e1.employee_id = e2.reports_to
