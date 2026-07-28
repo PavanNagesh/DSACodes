9ed07d7b3083ff83c860daafa44be827f753d204
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-07-28 11:50:41 +0530

    Time: 618 ms (30.1%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,7 +1,7 @@
 SELECT employee_id, department_id
 FROM Employee
 WHERE primary_flag = 'Y' OR employee_id IN (
-    SELECT employee_id FROM Employee
-    GROUP BY employee_id 
-    HAVING count(employee_id) = 1
-)
+        SELECT employee_id FROM Employee
+        GROUP BY employee_id 
+        HAVING count(employee_id) = 1
+    )
