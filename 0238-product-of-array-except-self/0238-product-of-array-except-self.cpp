e33fa1b64dc8ed08eaffd072ffb40c43fe55bdda
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-08-02 18:45:52 +0530

    Time: 3 ms (37.5%), Space: 40.1 MB (96.26%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -4,10 +4,12 @@ public:
         int n = nums.size();
         vector<int> product (n,1);
 
+        //prod_left
         for(int i = 1; i < n; i++){
             product[i] = product[i-1] * nums[i-1];
         }
 
+        //prod_right
         int suf = 1;
         for(int j = n-2; j >= 0; j--){
             suf = suf * nums[j+1];
