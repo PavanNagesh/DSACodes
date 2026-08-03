09a206158b61f8789786c45791dc2ba768c41c6d
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-08-03 19:14:59 +0530

    Time: 516 ms (46.97%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,7 @@
-SELECT w1.id
-FROM Weather w1, Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
-AND w1.temperature  > w2.temperature;
+SELECT id
+FROM(
+    SELECT id, recordDate, temperature, LAG(temperature) OVER(ORDER BY recordDate) as prev_temp, LAG(recordDate) OVER(ORDER BY recordDate) as prev_date
+    FROM Weather
+) as t
+WHERE DATEDIFF(recordDate, prev_date) = 1
+AND temperature > prev_temp;
