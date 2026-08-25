ceecdb7f36b184d561545e9a982c7c49b86e0ebd
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-08-25 08:32:37 +0530

    Time: 2 ms (5.1%), Space: 8.6 MB (39.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -9,7 +9,7 @@ public:
             return dp[n];
         }
 
-        return dp[n] = helper(n-1, dp   ) + helper(n-2, dp);
+        return dp[n] = helper(n-1, dp) + helper(n-2, dp);
     }
 
     int climbStairs(int n) {
