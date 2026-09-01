dea621e090100ad2025bcd9cbcc0d218dbab921d
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-09-01 19:39:05 +0530

    Time: 0 ms (100%), Space: 63 MB (49.27%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -3,15 +3,19 @@ public:
     int maxArea(vector<int>& height) {
         int lp = 0;
         int rp = height.size() - 1;
-        int h, w, area;
-        int maxarea = 0;
+        int ht, wt, area;
+        int maxArea = 0;
+
         while(lp < rp){
-            h = min(height[lp], height[rp]);
-            w = rp - lp;
-            area = w * h;
-            maxarea = max(maxarea, area);
+            ht = min(height[lp], height[rp]);
+            wt = rp - lp;
+            area = wt * ht;
+            maxArea = max(maxArea, area);
+            
             height[lp] < height[rp] ? lp++ : rp--;
+
         }
-        return maxarea;
+
+        return maxArea;
     }
 };
