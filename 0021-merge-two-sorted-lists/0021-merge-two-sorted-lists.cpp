faf167d3734d4fdb250aa46705686bacb626f7cc
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-09-03 13:18:57 +0530

    Time: 0 ms (100%), Space: 19.5 MB (62.82%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -15,12 +15,12 @@ public:
             return head1 == NULL? head2 : head1;
         }
 
-        // case1
-        if(head1->val <= head2->val){
-            head1->next = mergeTwoLists(head1->next, head2);
+        //case1
+        if(head1 -> val <= head2 -> val){
+            head1 -> next = mergeTwoLists(head1 -> next, head2);
             return head1;
-        } else{     // case2
-            head2->next = mergeTwoLists(head1, head2->next);
+        } else{
+            head2 -> next = mergeTwoLists(head1, head2 -> next);
             return head2;
         }
     }
