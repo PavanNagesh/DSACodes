d0ad710c1f6af2e8325a635cde9ee35bdf2a9e2f
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-09-17 06:25:23 +0530

    Time: 0 ms (100%), Space: 12.8 MB (77.94%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -18,6 +18,7 @@ public:
 
         int isLeftSame = isSameTree(p -> left, q -> left);
         int isRightSame = isSameTree(p -> right, q -> right);
+
         return isLeftSame && isRightSame && p -> val == q -> val;
     }
 };
