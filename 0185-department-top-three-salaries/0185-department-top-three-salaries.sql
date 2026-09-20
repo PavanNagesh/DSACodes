ff9b28a62280fff03c91d069784ff8776db85880
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-09-21 03:45:06 +0530

    Time: 1092 ms (60.42%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,8 +1,9 @@
 SELECT Department, Employee, Salary
 FROM(
-    SELECT d.name as Department, e.name as Employee, e.salary as Salary, DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary DESC) as rank_sal
-    FROM Employee as e
-    JOIN Department as d
+    SELECT d.name AS Department, e.name AS Employee, salary AS Salary, DENSE_RANK() OVER(PARTITION BY d.id ORDER BY salary DESC) AS ranked
+    FROM Employee e
+    JOIN Department d
     ON e.departmentId = d.id
-) as t
-WHERE rank_sal <= 3;
+    ORDER BY salary DESC
+) m
+WHERE ranked <= 3
