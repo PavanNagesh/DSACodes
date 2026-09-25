a42e270c97c669dd7bfc880f5a60368aae5eeb5d
PavanNagesh <108990790+PavanNagesh@users.noreply.github.com>
2026-09-25 11:47:06 +0530

    Time: 3 ms (67.86%), Space: 16 MB (5.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -7,13 +7,16 @@ public:
 
         for(int i = 0; i < n; i++){
             int first = nums[i];
-            int sec = target - first;
-            if(m.find(sec) != m.end()){
-                ans.push_back(m[sec]);
+            int second = target - first;
+            if(m.find(second) != m.end()){
                 ans.push_back(i);
+                ans.push_back(m[second]);
+            } else{
+                m[first] = i;
             }
-            m[first] = i;
+            
         }
+
         return ans;
     }
 };
